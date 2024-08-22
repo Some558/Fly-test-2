e3f6febead12626053cbcc925ad5d87c587e7e8c
Some558 <167545573+Some558@users.noreply.github.com>
2024-08-22 23:31:47 +0900

    nullable追加

--- database/migrations/2024_07_17_045604_add_test_column.php
+++ database/migrations/2024_07_17_045604_add_test_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('test')->after('email');
+            $table->string('test')->nullable();
         });
     }
 
